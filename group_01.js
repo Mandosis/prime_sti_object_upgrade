d291132e2902bab67f7d96d6dc22a3473deb32ed
Chris Rabuse <rab233@live.com>
2016-03-26 13:24:18 -0500

    Upgraded arrays to objects

--- group_01.js
+++ group_01.js
@@ -1,20 +1,27 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+var Employee = function(name, employeeId, income, rating) {
+  this.name = name;
+  this.id = employeeId;
+  this.income = income;
+  this.rating = rating;
+};
+
+var atticus = new Employee("Atticus", "2405", "47000", 3)
+var jem = new Employee("Jem", "62347", "63500", 4);
+var boo = new Employee("Boo", "11435", "54000", 3);
+var scout = new Employee("Scout", "6243", "74750", 5);
 
 var employees = [atticus, jem, boo, scout];
+
+calcSti(employees);
 
 function calcSti(employee) {
   var result = [];
   for(var i = 0; i < employee.length;i++){
     // Setting employee name
+    result[0] = employee[i].name;
 
-    result[0] = employee[i][0];
-
-     // Checking employee rating
-
-    switch (employee[i][3]) {
+    // Checking employee rating and setting bonus
+    switch (employee[i].rating) {
       case 3:
         result[1]=.04;
         break;
@@ -28,36 +35,43 @@ function calcSti(employee) {
         result[1]= 0;
       }
 
-      // Additional bonus % based on length at company
-      if (employee[i][1].length <= 4) {
+      // Additional bonus based on length of time at company
+      if (employee[i].id.length <= 4) {
         result[1] += .05;
       }
 
       // Remove 1% if income is over $65,000
-      if (employee[i][2] > 65000) {
+      if (employee[i].income > 65000) {
         result[1] -= .01;
       }
 
-      // If bonus is over 13% lower set it to 13%
+      // If bonus is over 13% lower it to 13%
       if (result[1] > .13) {
         result[1] = .13;
       }
 
       // Annual Income + STI
-      var sti = parseInt(employee[i][2])*result[1];  //calculates STI
-      result[2]= sti + parseInt(employee[i][2]); // adds income and STI
+      var sti = parseInt(employee[i].income)*result[1];  //calculates STI
+      result[2] = sti + parseInt(employee[i].income); // adds income and STI
       //console.log("Income + STI = " + result[2]);
 
       //Total bonus
       // Math.round()
-      result[3] = Math.round(employee[i][2] * result[1]);
+      result[3] = Math.round(employee[i].income * result[1]);
 
 
       // Convert decimal to percentage string with %
       result[1] = result[1] * 100 + "%";
 
+      // Build object
+      var employeeBonus = {
+        name: result[0],
+        percent: result[1],
+        income: result[2].toLocaleString('en-US', {style: 'currency', currency: 'USD', currencyDisplay: 'symbol'}),
+        bonus: result[3].toLocaleString('en-US', {style: 'currency', currency: 'USD', currencyDisplay: 'symbol'}),
+      };
+
       // Final result
-      console.log(result);
+      console.log(employeeBonus);
   }
 }
-calcSti(employees);
